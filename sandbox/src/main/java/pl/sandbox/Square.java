772018e52462d849600963cc55a9603b3b17e7ff
smokehead <artur.wojtowicz1988@gmail.com>
2018-02-20 21:25:57 +0100

    klasy

---
+++ sandbox/src/main/java/pl/sandbox/Square.java
@@ -0,0 +1,10 @@
+package pl.sandbox;
+
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+}
--- sandbox/src/main/java/pl/sandbox/hello.java
+++ sandbox/src/main/java/pl/sandbox/hello.java
@@ -6,23 +6,27 @@ public class hello {
     hello("world");
     hello("artur");
 
-    double l = 5;
-    System.out.println("powierzchnia kwadratu o boku " + l + " = " + area(l));
 
-    double a = 4;
-    double b = 6;
-    System.out.println("powierzchnia prostokatu o boku " + a + " i " + b +  " = " + area(a , b));
+    Square s = new Square(5);
+    //s.l = 5;
+    System.out.println("powierzchnia kwadratu o boku " + s.l + " = " + area(s));
+
+
+    Rectangle r = new Rectangle(4, 6);
+    //r.a = 4;
+    //r.b = 6;
+    System.out.println("powierzchnia prostokatu o boku " + r.a + " i " + r.b +  " = " + area(r));
   }
 	public static void hello(String somebody) {
 
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(double len) {
-	  return len * len;
+  public static double area(Square s) {
+	  return s.l * s.l;
   }
 
-  public static double area(double a, double b) {
-	  return a * b;
+  public static double area(Rectangle r) {
+	  return r.a * r.b;
   }
 }

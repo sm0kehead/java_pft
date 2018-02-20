ec79deee923e5ef40752ab91048ff2b3310e4434
smokehead <artur.wojtowicz1988@gmail.com>
2018-02-20 21:35:39 +0100

    Metody: Funkcje łaczone z obiektami

--- sandbox/src/main/java/pl/sandbox/hello.java
+++ sandbox/src/main/java/pl/sandbox/hello.java
@@ -9,13 +9,13 @@ public class hello {
 
     Square s = new Square(5);
     //s.l = 5;
-    System.out.println("powierzchnia kwadratu o boku " + s.l + " = " + area(s));
+    System.out.println("powierzchnia kwadratu o boku " + s.l + " = " + s.area());
 
 
     Rectangle r = new Rectangle(4, 6);
     //r.a = 4;
     //r.b = 6;
-    System.out.println("powierzchnia prostokatu o boku " + r.a + " i " + r.b +  " = " + area(r));
+    System.out.println("powierzchnia prostokatu o boku " + r.a + " i " + r.b +  " = " + r.area());
   }
 	public static void hello(String somebody) {
 

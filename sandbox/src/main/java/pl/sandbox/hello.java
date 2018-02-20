623ab875fbb5e1662e79874c82b05223f3e9a705
smokehead <artur.wojtowicz1988@gmail.com>
2018-02-20 20:49:16 +0100

    dodano kilka funkcji

--- sandbox/src/main/java/pl/sandbox/hello.java
+++ sandbox/src/main/java/pl/sandbox/hello.java
@@ -3,8 +3,26 @@ package pl.sandbox;
 public class hello {
 
 	public static void main(String[] args) {
+    hello("world");
+    hello("artur");
 
-		System.out.println("Hello, World");
+    double l = 5;
+    System.out.println("powierzchnia kwadratu o boku " + l + " = " + area(l));
 
-	}
+    double a = 4;
+    double b = 6;
+    System.out.println("powierzchnia prostokatu o boku " + a + " i " + b +  " = " + area(a , b));
+  }
+	public static void hello(String somebody) {
+
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+	  return len * len;
+  }
+
+  public static double area(double a, double b) {
+	  return a * b;
+  }
 }

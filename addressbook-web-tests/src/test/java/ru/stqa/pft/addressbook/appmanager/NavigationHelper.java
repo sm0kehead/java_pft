8ed270a2d9528579c5666f7af973a5d735a3fe4a
smokehead <artur.wojtowicz1988@gmail.com>
2018-03-01 22:25:26 +0100

    Struktura 2 poziomowa fix zdublowanego firefoxa

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class NavigationHelper extends HelperBase {
-  private FirefoxDriver wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:/Program Files/Mozilla Firefox/firefox.exe"));
+  //private FirefoxDriver wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:/Program Files/Mozilla Firefox/firefox.exe"));
 
   public NavigationHelper(FirefoxDriver wd) {
     super(wd);
